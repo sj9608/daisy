b15d6d31a0b69ef7f7f6c7067d829bc538745927
seungjun <sneak7581@wku.ac.kr>
2021-07-06 18:27:13 +0900

    프로젝트 매니저 재구상중

--- src/views/todo.vue
+++ src/views/todo.vue
@@ -1,242 +1,54 @@
 <template>
-    <div></div>
-  <!-- <header class="header">
-    <h1>todos</h1>
-    <input
-           class="new-todo"
-           autofocus
-           autocomplete="off"
-           placeholder="What needs to be done?"
-           v-model="newTodo"
-           @keyup.enter="addTodo"
-           />
-  </header>
-  <section class="main" v-show="todos.length" v-cloak>
-    <input
-           id="toggle-all"
-           class="toggle-all"
-           type="checkbox"
-           v-model="allDone"
-           />
-    <label for="toggle-all"></label>
-    <ul class="todo-list">
-      <li
-          v-for="todo in filteredTodos"
-          class="todo"
-          :key="todo.id"
-          :class="{ completed: todo.completed, editing: todo == editedTodo }"
-          >
-        <div class="view">
-          <input class="toggle" type="checkbox" v-model="todo.completed" />
-          <label @dblclick="editTodo(todo)">{{ todo.title }}</label>
-          <button class="destroy" @click="removeTodo(todo)"></button>
-        </div>
-        <input
-               class="edit"
-               type="text"
-               v-model="todo.title"
-               v-todo-focus="todo == editedTodo"
-               @blur="doneEdit(todo)"
-               @keyup.enter="doneEdit(todo)"
-               @keyup.esc="cancelEdit(todo)"
-               />
+  <div>
+    <h1>Project List</h1>
+
+    <ul id="prj_list">
+      <li @click="st2dom" v-for="item in items" :key="item">
+        {{ item.name }}
       </li>
     </ul>
-  </section>
-  <footer class="footer" v-show="todos.length" v-cloak>
-    <span class="todo-count">
-      <strong>{{ remaining }}</strong> {{ remaining | pluralize }} left
-    </span>
-    <ul class="filters">
-      <li>
-        <a href="#/all" :class="{ selected: visibility == 'all' }">All</a>
-      </li>
-      <li>
-        <a href="#/active" :class="{ selected: visibility == 'active' }">Active</a>
-      </li>
-      <li>
-        <a
-           href="#/completed"
-           :class="{ selected: visibility == 'completed' }">Completed</a>
-      </li>
-    </ul>
-    <button
-            class="clear-completed"
-            @click="removeCompleted"
-            v-show="todos.length > remaining"
-            >
-      Clear completed
-    </button>
-  </footer>
-</section>
-<footer class="info">
-  <p>Double-click to edit a todo</p>
-  <p>Written by <a href="http://evanyou.me">Evan You</a></p>
-  <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-</footer> -->
+
+    
+  </div>
 </template>
 
-
 <script>
-// // Full spec-compliant TodoMVC with localStorage persistence
-// // and hash-based routing in ~120 effective lines of JavaScript.
+export default {
+  data() {
+    return {
+      items: [],
+      configData: {
+        prjs: {}
+      },
+    };
+  },
 
-// // localStorage persistence
-// const STORAGE_KEY = "todos-vuejs-2.0";
-// const todoStorage = {
-//   fetch() {
-//     const todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]");
-//     todos.forEach((todo, index) => {
-//       todo.id = index;
-//     });
-//     todoStorage.uid = todos.length;
-//     return todos;
-//   },
-//   save(todos) {
-//     localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
-//   },
-// };
+  methods: {
+    st2dom(evt) {
+      console.log(evt.target.innerText)
+    },
+  },
 
-// // visibility filters
-// const filters = {
-//   all(todos) {
-//     return todos;
-//   },
-//   active(todos) {
-//     return todos.filter((todo) => !todo.completed);
-//   },
-//   completed(todos) {
-//     return todos.filter((todo) => todo.completed);
-//   },
-// };
+  async mounted() {
 
-// // app Vue instance
-// const app = Vue.createApp({
-//   // app initial state
-//   data() {
-//     return {
-//       todos: todoStorage.fetch(),
-//       newTodo: "",
-//       editedTodo: null,
-//       visibility: "all",
-//     };
-//   },
+    try {
+      let port = 21030,
+      configData = await (await fetch(`http://gears001.iptime.org:${port}/rest/download?filepath=./__config.json`)).json();
+      console.log(configData);
 
-//   // watch todos change for localStorage persistence
-//   watch: {
-//     todos: {
-//       handler(todos) {
-//         todoStorage.save(todos);
-//       },
-//       deep: true,
-//     },
-//   },
-
-//   // computed properties
-//   // http://vuejs.org/guide/computed.html
-//   computed: {
-//     filteredTodos() {
-//       return filters[this.visibility](this.todos);
-//     },
-//     remaining() {
-//       return filters.active(this.todos).length;
-//     },
-//     allDone: {
-//       get() {
-//         return this.remaining === 0;
-//       },
-//       set(value) {
-//         this.todos.forEach((todo) => {
-//           todo.completed = value;
-//         });
-//       },
-//     },
-//   },
-
-//   // methods that implement data logic.
-//   // note there's no DOM manipulation here at all.
-//   methods: {
-//     pluralize(n) {
-//       return n === 1 ? "item" : "items";
-//     },
-//     addTodo() {
-//       var value = this.newTodo && this.newTodo.trim();
-//       if (!value) {
-//         return;
-//       }
-//       this.todos.push({
-//         id: todoStorage.uid++,
-//         title: value,
-//         completed: false,
-//       });
-//       this.newTodo = "";
-//     },
-
-//     removeTodo(todo) {
-//       this.todos.splice(this.todos.indexOf(todo), 1);
-//     },
-
-//     editTodo(todo) {
-//       this.beforeEditCache = todo.title;
-//       this.editedTodo = todo;
-//     },
-
-//     doneEdit(todo) {
-//       if (!this.editedTodo) {
-//         return;
-//       }
-//       this.editedTodo = null;
-//       todo.title = todo.title.trim();
-//       if (!todo.title) {
-//         this.removeTodo(todo);
-//       }
-//     },
-
-//     cancelEdit(todo) {
-//       this.editedTodo = null;
-//       todo.title = this.beforeEditCache;
-//     },
-
-//     removeCompleted() {
-//       this.todos = filters.active(this.todos);
-//     },
-//   },
-
-//   // a custom directive to wait for the DOM to be updated
-//   // before focusing on the input field.
-//   // http://vuejs.org/guide/custom-directive.html
-//   directives: {
-//     "todo-focus": {
-//       updated(el, binding) {
-//         if (binding.value) {
-//           el.focus();
-//         }
-//       },
-//     },
-//   },
-// });
-
-// // mount
-// const vm = app.mount(".todoapp");
-
-// // handle routing
-// function onHashChange() {
-//   const visibility = window.location.hash.replace(/#\/?/, "");
-//   if (filters[visibility]) {
-//     vm.visibility = visibility;
-//   } else {
-//     window.location.hash = "";
-//     vm.visibility = "all";
-//   }
-// }
-
-// window.addEventListener("hashchange", onHashChange);
-// onHashChange();
-
-// export default {};
+      for (let name in configData.prjs) {
+        console.log(name);
+        this.items.push({name:name})
+      }
+    }
+    catch(e) {
+      console.log(e);
+    }
+  },
+};
 </script>
+
 <style>
-/* [v-cloak] {
-  display: none;
-} */
+@import "../assets/scss/core.scss";
+@import "../assets/scss/prjmng.scss";
 </style>
